8ba8dd74de1a44a110586a564a5da5bbc2997b35
Xin (Cyndi) Ai <aixin@cs.washington.edu>
2016-05-14 17:26:04 -0700

    changing gradle to have junit working

---
+++ WalkBuddies2/app/src/androidTest/java/wehack/tune/walkbuddies/Data/LocationHandlerTest.java
@@ -0,0 +1,11 @@
+package wehack.tune.walkbuddies.Data;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Created by aixin on 5/14/16.
+ */
+public class LocationHandlerTest {
+
+}
